e33441f6ee0cf373b8bcb11d739ca7585d71e906
taeyoung <127275174+h2oterry@users.noreply.github.com>
2023-09-21 15:14:05 +0900

    feat : Sign 라우터 추가

--- src/router/MainRouter.tsx
+++ src/router/MainRouter.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Main from "../pages/Main";
 import RootLayout from "../components/RootLayout";
+import Sign from "../pages/Sign";
 
 const MainRouter = () => {
   return (
     <Routes>
+      <Route path="sign" element={<Sign />} />
       <Route path="/" element={<RootLayout />}>
         <Route path="/" element={<Main />} />
       </Route>
