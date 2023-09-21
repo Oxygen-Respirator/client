6452425b2f70a77adc5d4c123d231be393d28c70
taeyoung <127275174+h2oterry@users.noreply.github.com>
2023-09-21 15:12:28 +0900

    feat : theme 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { BrowserRouter } from 'react-router-dom';
-import MainRouter from './router/MainRouter';
-import GlobalStyle from './styles/GlobalStyle';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter } from "react-router-dom";
+import MainRouter from "./router/MainRouter";
+import GlobalStyle from "./styles/GlobalStyle";
+import { ThemeProvider } from "styled-components";
 
 function App() {
   const queryClient = new QueryClient();
 
+  const theme = {
+    color: {
+      mainColor: "#18CD8C",
+      subColor: "#F4FEF6",
+      borderColor: "#C1C1C1",
+    },
+  };
+
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <GlobalStyle />
-        <MainRouter />
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <MainRouter />
+        </ThemeProvider>
       </BrowserRouter>
     </QueryClientProvider>
   );
