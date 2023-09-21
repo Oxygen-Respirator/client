b5e9d4eac42641fcd6fe6ed41abb8508c99e6a38
taeyoung <127275174+h2oterry@users.noreply.github.com>
2023-09-21 16:12:33 +0900

    feat : border-color, border-radius 값 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,28 @@ import MainRouter from "./router/MainRouter";
 import GlobalStyle from "./styles/GlobalStyle";
 import { ThemeProvider } from "styled-components";
 
+interface Theme {
+  color: {
+    mainColor: "#18CD8C";
+    subColor: "#F4FEF6";
+    borderColor: "#C1C1C1";
+  };
+  border: {
+    defaultRadius: "20px";
+  };
+}
+
 function App() {
   const queryClient = new QueryClient();
 
-  const theme = {
+  const theme: Theme = {
     color: {
       mainColor: "#18CD8C",
       subColor: "#F4FEF6",
       borderColor: "#C1C1C1",
+    },
+    border: {
+      defaultRadius: "20px",
     },
   };
 
