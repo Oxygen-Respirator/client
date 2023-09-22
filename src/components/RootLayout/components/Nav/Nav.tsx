8052ecccb6e35d158f83be9bda8207d94fec8ee7
YoujungSon <andn1026@gmail.com>
2023-09-22 21:27:52 +0900

    feat: nav 수정

--- src/components/RootLayout/components/Nav/Nav.tsx
+++ src/components/RootLayout/components/Nav/Nav.tsx
@@ -1,16 +1,21 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import styled from "styled-components";
-import { deleteCookie, getCookie } from "@/utils/cookie";
+import { deleteCookie } from "@/utils/cookie";
 import { LogoIcon } from "@/components/Svg";
 import Sign from "./Sign";
 
 const Nav = () => {
+  const params = useParams();
   const [isSignModal, setIsSignModal] = useState<IsSignModal>({
     in: false,
     up: false,
   });
-
+  const links = [
+    { to: "/", text: "AI 모의면접" },
+    { to: "/rank", text: "랭킹" },
+    { to: "/report", text: "학습 리포트" },
+  ];
   const [isLogin, setIsLogin] = useState(false);
 
   useEffect(() => {
@@ -25,52 +30,48 @@ const Nav = () => {
       <LogoIconWrap to="/">
         <LogoIcon />
       </LogoIconWrap>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">AI 모의면접</Link>
-          </li>
-          <li>
-            <Link to="/">랭킹</Link>
-          </li>
-          <li>
-            <Link to="/">학습 리포트</Link>
-          </li>
-        </ul>
-      </div>
-      <div>
-        {isLogin ? (
-          <Row>
-            <p>아리(Ari) 님 반가워요!</p>
-            <Button
-              type="button"
-              onClick={() => {
-                deleteCookie("token");
-                location.href = "/";
-              }}
-            >
-              로그아웃
-            </Button>
-          </Row>
-        ) : (
-          <Row>
-            <Button
-              onClick={() => {
-                setIsSignModal(prev => ({ ...prev, in: true }));
-              }}
-            >
-              로그인
-            </Button>
-            <Button
-              onClick={() => {
-                setIsSignModal(prev => ({ ...prev, up: true }));
-              }}
-            >
-              회원가입
-            </Button>
-          </Row>
-        )}
-      </div>
+      <RowBetwwen>
+        <LinkList>
+          {links.map((link, index) => (
+            <LinkItem key={index}>
+              <Link to={link.to}>{link.text}</Link>
+            </LinkItem>
+          ))}
+        </LinkList>
+        <div>
+          {isLogin ? (
+            <Row>
+              <p>아리(Ari) 님 반가워요!</p>
+              <Button
+                type="button"
+                onClick={() => {
+                  deleteCookie("token");
+                  location.href = "/";
+                }}
+              >
+                로그아웃
+              </Button>
+            </Row>
+          ) : (
+            <Row>
+              <Button
+                onClick={() => {
+                  setIsSignModal(prev => ({ ...prev, in: true }));
+                }}
+              >
+                로그인
+              </Button>
+              <Button
+                onClick={() => {
+                  setIsSignModal(prev => ({ ...prev, up: true }));
+                }}
+              >
+                회원가입
+              </Button>
+            </Row>
+          )}
+        </div>
+      </RowBetwwen>
       {<Sign isSignModal={isSignModal} setIsSignModal={setIsSignModal} />}
     </HeaderContainer>
   );
@@ -81,10 +82,8 @@ export default Nav;
 const HeaderContainer = styled.div`
   width: 100%;
   display: flex;
-  align-items: center;
-  justify-content: space-between;
   height: 60px;
-  padding: 1rem 2rem;
+  padding: 1rem 0;
   box-shadow: 0 1px 15px rgba(0, 0, 0, 0.04), 0 1px 6px rgba(0, 0, 0, 0.04);
 `;
 
@@ -92,6 +91,10 @@ const LogoIconWrap = styled(Link)`
   font-weight: bold;
   color: ${({ theme: { color } }) => color.mainColor};
   font-size: 18px;
+  width: 15%;
+  min-width: 220px;
+  padding: 0 2rem;
+  display: block;
 `;
 
 const Button = styled.button`
@@ -100,7 +103,6 @@ const Button = styled.button`
   justify-content: center;
   align-items: center;
   gap: 8px;
-  color: #474747;
   border: 1px solid #d7d7d7;
   border-radius: 8px;
   background-color: #fff;
@@ -111,4 +113,29 @@ const Row = styled.div`
   display: flex;
   align-items: center;
   gap: 1rem;
+`;
+
+const LinkList = styled.ul`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`;
+const LinkItem = styled.li`
+  display: block;
+  width: 130px;
+  text-align: center;
+  a {
+    width: 100%;
+    height: 100%;
+    display: block;
+    padding: 1rem;
+  }
+`;
+
+const RowBetwwen = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-right: 2rem;
 `;
