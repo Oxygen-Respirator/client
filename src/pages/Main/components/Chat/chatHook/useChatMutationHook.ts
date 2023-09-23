5237bc48d310d177414c405d0fa8df5796076099
taeyoung <hty3466@naver.com>
2023-09-23 17:40:49 +0900

    code&css 정리 feat.ari

--- src/pages/Main/components/Chat/chatHook/useChatMutationHook.ts
+++
@@ -1,5 +0,0 @@
-import chatApis from "@/apis/chatApis";
-import { useMutation } from "@tanstack/react-query";
-
-
-export function useSendChatMutation(){return useMutation((msg:string)=>chatApis.send(msg))}
