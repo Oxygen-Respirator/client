b17689f08d28650a40519837bdf904ca1688ba06
taeyoung <hty3466@naver.com>
2023-09-23 14:00:33 +0900

    feat : 토큰 만료 로직

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -4,18 +4,34 @@ const instance = axios.create({ baseURL: "https://api.oxygen-ai.site/api/" });
 
 export default instance;
 
-
 instance.interceptors.request.use(
-    config => {
-      const token = localStorage.getItem("ptToken");
-      if (token) {
-        config.headers["authorization"] = `Bearer ${token}`;
-        return config;
-      }
+  config => {
+    const token = localStorage.getItem("ptToken");
+    if (token) {
+      config.headers["authorization"] = `Bearer ${token}`;
       return config;
-    },
-    error => {
-      return error;
-    },
-  );
-  
+    }
+    return config;
+  },
+  error => {
+    return error;
+  },
+);
+
+instance.interceptors.response.use(
+  response => response,
+  error => {
+    if (
+      error.response &&
+      (error.response.status === 401 || error.response.status === 403)
+    ) {
+      localStorage.removeItem("ptToken");
+      alert("다시 로그인해 주세요");
+
+      window.location.href = "/";
+    } else {
+      console.log(error);
+    }
+    return Promise.reject(error);
+  },
+);
